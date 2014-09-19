a504f8fbc0a7fbbcfc805aa05e7b24e6a75238b4
Samantha McCormick <omgitzme@gmail.com>
2014-09-19 15:15:58 -0600

    Finished bonus

--- bonus.js
+++ bonus.js
@@ -5,16 +5,13 @@ $(document).on('ready', function() {
 	// On click of button with class "show", toggle the form (toggle = show/hide), replace text,
 	// and add a class of "hide"
 	$(document).on("click", ".show", function() {
-
 		$(".formContainer").toggle();
 		$(this).text("Hide Form").addClass("hide").removeClass("show");
-
 	});
 
 	// On click of an element with both "show" and "hide" classes, toggle the form again, 
 	// then remove the "hide" class so that you can once again use the above 'hide' event handler
 	$(document).on("click", ".hide", function() {
-
 		$(".formContainer").toggle();
 		$(this).text("Show Form").addClass("show").removeClass("hide");
 	});
@@ -26,25 +23,21 @@ $(document).on('ready', function() {
 
 	// On blur of a textarea...
 	$(document).on("blur", "textarea", function() {
-		$(this).show();   /* When blurred, show user what they entered */
-	});
 
-	// On click of submit button
-	$(document).on("click", ".submit", function() {
-		// On submit, save all value entries into their own variables. This must be donw within this event!
+		// Save all value entries into their own variables. This must be done within this event!
 		var nameEntry = $(".name").val();  
 		var bioEntry = $(".bio").val(); 
 		var booksEntry = $(".books").val();
 		var jsEntry = $(".js_libraries").val();
 
-		$(".submit").closest(".formContainer").hide();
+		// Show each entry in its display field
+		$(".name").closest(".container").find(".name").text(nameEntry);
+		$(".bio").closest(".container").find(".bio").text(bioEntry);
+		$(".books").closest(".container").find(".books").text(booksEntry);
+		$(".js_libraries").closest(".container").find(".js_libraries").text(jsEntry);
 
-		/* Take each entry and put it into the corresponding class location */
-		$(".name").replaceWith(nameEntry);
-		$(".bio").replaceWith(bioEntry);
-		$(".books").replaceWith(booksEntry);
-		$(".js_libraries").replaceWith(jsEntry);
 	});
+
 
 
 });
